f0b3fc7cd9b6578736037548b99843137e4abc1c
ZhangHongxu <diguamail@gmail.com>
2011-10-06 22:27:36 +0800

    remove notes

--- MyFirstIos/MyFirstIosAppDelegate.m
+++ MyFirstIos/MyFirstIosAppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"========================inter2");
+   
     
     
     MyViewController *aViewController = [[MyViewController alloc]
